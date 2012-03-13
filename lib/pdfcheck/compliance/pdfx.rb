5cfd20819ab4f9334eb16fd6e570b1bd55d58889
Oliver Brooks <oliver@completelynovel.com>
2012-03-13 12:34:51 +0000

    add pdfxa compliance type to pdfx

--- lib/pdfcheck/compliance/pdfx.rb
+++ lib/pdfcheck/compliance/pdfx.rb
@@ -5,7 +5,7 @@ module PDFcheck
       @report[:fonts_embedded]   = check @reader.fonts_embedded?
       @report[:no_rgb]           = check !@reader.rgb?
       @report[:pdf_version]      = check @reader.pdf_version, "1.3"
-      @report[:intent]           = check_in @reader.metadata.output_intent, ["GTS_PDFX"]
+      @report[:intent]           = check_in @reader.metadata.output_intent, ["GTS_PDFX", "GTS_PDFX-A"]
       @report[:unencrypted]      = check !@reader.security.encrypted?
       @report[:printing_allowed] = check @reader.security.printing_allowed?
     end
